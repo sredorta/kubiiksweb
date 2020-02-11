bc46e3c9c68e35a99a9141f6907d9b2067058ef1
sredorta <sergi.redorta@hotmail.com>
2020-02-11 15:03:04 +0100

    Created admin menu

--- src/app/_features/admin/routes/kii-admin-menu/kii-admin-menu.component.ts
+++ src/app/_features/admin/routes/kii-admin-menu/kii-admin-menu.component.ts
@@ -1,18 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { KiiTranslateService } from 'src/app/_features/translate/services/kii-translate.service';
+import { KiiBaseAbstract } from 'src/app/abstracts/kii-base.abstract';
+import { KiiAuthService } from 'src/app/_features/main/services/kii-auth.service';
+import { User } from 'src/app/_features/main/models/user';
+import { faUserCog } from '@fortawesome/free-solid-svg-icons/faUserCog';
+import { faCogs } from '@fortawesome/free-solid-svg-icons/faCogs';
+import { faPenNib } from '@fortawesome/free-solid-svg-icons/faPenNib';
+import { faAt } from '@fortawesome/free-solid-svg-icons/faAt';
+import { faComments } from '@fortawesome/free-solid-svg-icons/faComments';
+import { faWindowRestore } from '@fortawesome/free-solid-svg-icons/faWindowRestore';
+import { faSave } from '@fortawesome/free-solid-svg-icons/faSave';
+import { faChartLine } from '@fortawesome/free-solid-svg-icons/faChartLine';
+
+
 
 @Component({
   selector: 'kii-admin-menu',
   templateUrl: './kii-admin-menu.component.html',
   styleUrls: ['./kii-admin-menu.component.scss']
 })
-export class KiiAdminMenuComponent implements OnInit {
+export class KiiAdminMenuComponent extends KiiBaseAbstract implements OnInit {
 
-  constructor(private kiiTrans: KiiTranslateService, private location : Location) { }
+  loggedInUser : User = new User(null);
+  icons = [];
+
+  constructor(
+    private kiiTrans: KiiTranslateService, 
+    private location : Location,
+    private kiiAuth: KiiAuthService
+    ) { 
+      super();
+      this.icons['settings'] = faCogs;
+      this.icons['users'] = faUserCog;
+      this.icons['content'] = faPenNib;
+      this.icons['emails'] = faAt;
+      this.icons['chats'] = faComments;
+      this.icons['popup'] = faWindowRestore;
+      this.icons['disk'] = faSave;
+      this.icons['stats'] = faChartLine;
+ 
+
+    }
 
   ngOnInit() {
     this.kiiTrans.setRequiredContext(['admin']);
+    this.addSubscriber(
+      this.kiiAuth.getLoggedInUser().subscribe(res => {
+        this.loggedInUser = res;
+      })
+    )
   }
 
 
--- src/app/_features/main/models/user.ts
+++ src/app/_features/main/models/user.ts
@@ -110,10 +110,16 @@ export class User {
 
     /**Returns if user has specified role */
     public hasRole(role:number | string) : boolean {
+        let hasRole : boolean = false;
         if (typeof role == "string") {
-            return this.roles.find(obj => obj.name == role) == undefined?false:true;
+            hasRole = this.roles.find(obj => obj.name == role) == undefined?false:true;
+            //Check if user is admin and return as it had the role
+            if (!hasRole) hasRole = this.roles.find(obj => obj.name == "admin") == undefined?false:true;
+            return hasRole;
         } else {
-            return this.roles.find(obj => obj.id == role) == undefined?false:true;
+            hasRole = this.roles.find(obj => obj.id == role) == undefined?false:true;
+            if (!hasRole) hasRole = this.roles.find(obj => obj.name == "admin") == undefined?false:true;
+            return hasRole;
         }
     }
     /**Returns if user has any special role */
