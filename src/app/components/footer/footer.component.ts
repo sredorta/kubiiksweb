923a51d423bd225b15ea65796001fcf590e68f55
sredorta <sergi.redorta@hotmail.com>
2020-02-13 17:41:36 +0100

    Working on footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,6 +3,8 @@ import { isPlatformBrowser } from '@angular/common';
 import { KiiMainUserService } from 'src/app/_features/main/services/kii-main-user.service';
 import { User } from 'src/app/_features/main/models/user';
 import { KiiBaseAbstract } from 'src/app/abstracts/kii-base.abstract';
+import { KiiMainSettingService } from 'src/app/_features/main/services/kii-main-setting.service';
+import { Setting } from 'src/app/_features/main/models/setting';
 
 @Component({
   selector: 'app-footer',
@@ -11,12 +13,32 @@ import { KiiBaseAbstract } from 'src/app/abstracts/kii-base.abstract';
 })
 export class FooterComponent extends KiiBaseAbstract implements OnInit {
   loggedInUser : User = new User(null);
-  constructor( private kiiAuth: KiiMainUserService) { super()}
+  phone:string = "";
+  email:string = "";
+  addressStreet:string= "";
+  addressPostal:string= "";
+  addressLocality:string="";
+  addressCountry:string="";
+
+  constructor( private kiiAuth: KiiMainUserService, private kiiSettings: KiiMainSettingService) { super()}
 
   ngOnInit() {
     this.addSubscriber(
       this.kiiAuth.getLoggedInUser().subscribe(res => {
         this.loggedInUser = res;
+      })
+    )
+    this.addSubscriber(
+      this.kiiSettings.onChange.subscribe(res => {
+        this.phone = Setting.getByKey('telephone',this.kiiSettings.getValue()).value;
+        this.email = Setting.getByKey('email',this.kiiSettings.getValue()).value;
+        this.addressStreet = Setting.getByKey('addressStreet',this.kiiSettings.getValue()).value;
+        this.addressPostal = Setting.getByKey('addressPostal',this.kiiSettings.getValue()).value;
+        this.addressLocality = Setting.getByKey('addressLocality',this.kiiSettings.getValue()).value;
+        this.addressCountry = Setting.getByKey('addressCountry',this.kiiSettings.getValue()).value;
+        //TODO: MOVE THIS PART IN KII-FOOTER AND PASS AS ARGUMENT !
+        console.log("RECIEVED PHONE:",this.phone);
+
       })
     )
   }
