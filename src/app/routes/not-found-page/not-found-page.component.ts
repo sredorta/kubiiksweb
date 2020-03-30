f0a49b21806ad53370a4531bcfe9d54da51d64f0
sredorta <sergi.redorta@hotmail.com>
2020-03-30 15:24:31 +0200

    Add 404 on not found if we are server

--- src/app/routes/not-found-page/not-found-page.component.ts
+++ src/app/routes/not-found-page/not-found-page.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Optional, Inject, PLATFORM_ID } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { KiiBaseAbstract } from 'src/app/abstracts/kii-base.abstract';
 import { Article } from 'src/app/_features/main/models/article';
 import { KiiMainDataService } from 'src/app/_features/main/services/kii-main-data.service';
 import { KiiMainArticleService } from 'src/app/_features/main/services/kii-main-article.service';
 import { KiiTranslateService } from 'src/app/_features/translate/services/kii-translate.service';
+import { RESPONSE } from '@nguniversal/express-engine/tokens';
+import { Response } from 'express';
+import { isPlatformServer } from '@angular/common';
 
 
 @Component({
@@ -22,15 +25,18 @@ export class NotFoundPageComponent extends KiiBaseAbstract implements OnInit {
   showCreated : boolean = false;
 
   constructor(
-      private route: ActivatedRoute, 
-      private data : KiiMainDataService,
-      private articles: KiiMainArticleService,
+      @Optional() @Inject(RESPONSE) private response: Response,
+      @Inject(PLATFORM_ID) private platformId: any,
       private trans: KiiTranslateService,
       private router : Router
               
             ) { super()}
 
   ngOnInit() {
+    //Return 404 code if we are the server
+    if (isPlatformServer(this.platformId)) {
+      this.response.status(404);
+    }
     this.trans.setRequiredContext(['main','not-found']);
 
   }
